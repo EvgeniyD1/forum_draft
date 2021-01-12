8f825a01057e30bc4cd981b2f8e98ed2dc791f49
EvgeniyD1 <edolgiyj7@gmail.com>
2021-01-12 20:24:33 +0300

    role and macro

--- src/main/java/com/forum/forum_draft/domain/Role.java
+++ src/main/java/com/forum/forum_draft/domain/Role.java
@@ -3,7 +3,7 @@ package com.forum.forum_draft.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
